5f3df278cc7094b39256083910f4e49adea62358
Ashley lamont <alamont@atlassian.com>
2025-09-17 15:47:07 +1000

    Refactor nvmrc loading logic and enable corepack for package manager shims

--- home/core.nix
+++ home/core.nix
@@ -191,22 +191,24 @@
             autoload -U add-zsh-hook
 
             load-nvmrc() {
-            local nvmrc_path
-            nvmrc_path="$(nvm_find_nvmrc)"
+                local nvmrc_path
+                nvmrc_path="$(nvm_find_nvmrc)"
 
-            if [ -n "$nvmrc_path" ]; then
-                local nvmrc_node_version
-                nvmrc_node_version=$(nvm version "$(cat "''${nvmrc_path}")")
+                if [ -n "$nvmrc_path" ]; then
+                    local nvmrc_node_version
+                    nvmrc_node_version=$(nvm version "$(cat "''${nvmrc_path}")")
 
-                if [ "$nvmrc_node_version" = "N/A" ]; then
-                nvm install
-                elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
-                nvm use
+                    if [ "$nvmrc_node_version" = "N/A" ]; then
+                    nvm install
+                    elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
+                    nvm use
+                    fi
+                elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
+                    echo "Reverting to nvm default version"
+                    nvm use default
                 fi
-            elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
-                echo "Reverting to nvm default version"
-                nvm use default
-            fi
+                # Finally, enable corepack to get yarn/pnpm/bun shims working
+                corepack enable
             }
 
             add-zsh-hook chpwd load-nvmrc
