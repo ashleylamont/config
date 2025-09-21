65176626a7c011fac151e00b55d6b77a71099dde
Ashley Lamont <ashley@ashl.dev>
2025-09-21 18:58:53 +1000

    Fix nvm

--- home/core.nix
+++ home/core.nix
@@ -185,6 +185,14 @@
                 source "$HOME/.cargo/env"
             fi
 
+            # Install nvm if the command is not found
+            if [ -z "$(command -v nvm)" ]; then
+                curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+            fi
+            export NVM_DIR="$HOME/.nvm"
+            [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+            [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
             # nvm auto-use
             autoload -U add-zsh-hook
 
